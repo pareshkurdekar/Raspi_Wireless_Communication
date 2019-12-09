a297f3dbea21cf4c532c16a1a7c4758cf2491c39
pareshkurdekar <48374437+pareshkurdekar@users.noreply.github.com>
2019-12-09 16:09:34 +0530

    Update Rpi_Esp_Mqtt_tstcode.py

--- Rpi_Esp_Mqtt_tstcode.py
+++ Rpi_Esp_Mqtt_tstcode.py
@@ -1,39 +1,61 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
-import sys
+import time
+
 Master_subnode = mqtt.Client()
 
-host = '192.168.225.186'
+host =  "192.168.43.17"
 port = 1883
+ctr = 0
+
+pH = 0.0
+DO = 0.0
+Conductivity = 0.0
+sensor_value_list = []
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    Master_subnode.subscribe("esp8266")
+
 
     Master_subnode.subscribe("Sensor_values")               #Getting Sensors Values
+
     Master_subnode.subscribe("Actuation_ACK")
+
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    sys.exit()
+    s = msg.payload.decode("utf-8")
+    print(s,type(s))
+    if msg.topic == "Sensor_values":
+        diff_sensor(s)
 
-Master_subnode.connect(host,port,60)
+#Master_subnode.connect(host,port,60)
+
+def diff_sensor(s):
+    temp_list = list(s)
+    global sensor_value_list
+    for i in range(0,len(s)):
+        if s[i] is ",":
+            sensor_value_list += s[i - 1]
+    print(sensor_value_list)        
+
 
 Master_subnode.on_connect = on_connect
 Master_subnode.on_message = on_message
+Master_subnode.connect(host,port,60)
+Master_subnode.loop_start()
+#Master_subnode.subscribe("Sensor_Values")
 
 while True:
-    command = input("Enter Command")
-    print(type(command))
-        # Master_subnode.publish("esp8266",command)
+    time.sleep(0.5)
+    command = input("Enter Command :")
+
+
     if (command == "1"):
         Master_subnode.publish("Sensor_Read_Request",command)
-        print("sdsdsd")
+        time.sleep(6)
     if(command == "2"):
                                             #Decide which actuator to s$
                                             #Write it in the form of a $
         actuation_values = "1:On,2:Off,3:On,4:Off"
-        Master_subnode.publish("Actuation",actuation_values,host)
-
-                                                    #Change actuation_values if needed
-Master_subnode.loop_forever()
+        Master_subnode.publish("Actuation",actuation_values)
