82c7e503acff13f4c266f7e82718c7bed2561851
pareshkurdekar <48374437+pareshkurdekar@users.noreply.github.com>
2019-12-13 15:42:16 +0530

    Rpi_code

--- Rpi_Esp_Mqtt_tstcode.py
+++ Rpi_Esp_Mqtt_tstcode.py
@@ -1,10 +1,13 @@
+
+
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import time
 
+
 Master_subnode = mqtt.Client()
 
-host =  "192.168.43.17"
+host =  "192.168.225.239"
 port = 1883
 ctr = 0
 
@@ -13,6 +16,7 @@ DO = 0.0
 Conductivity = 0.0
 sensor_value_list = []
 
+actuation_values = ""
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
@@ -24,41 +28,63 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    #print(type(msg.payload))
     s = msg.payload.decode("utf-8")
-    print(s,type(s))
+    #print(s,type(s))
+ 
     if msg.topic == "Sensor_values":
         diff_sensor(s)
+    if msg.topic == "Actuation_ACK":
+        diff_actuators(s)
 
-#Master_subnode.connect(host,port,60)
+def diff_actuators(s):
 
+    global actuation_values
+    d = "".join(str(ord(c)) for c in s) 
+    #print(d)
+    if actuation_values == d:
+        print("Job Done")
+    
+    print("Acknowledged!")
 
 def diff_sensor(s):
+
+    
+
     temp_list = s.split(",")
-    temp_list[2] = temp_list[2].replace('\x00','')
+    temp_list[2] = temp_list[2].replace('\x00','')  
     #print(temp_list)
     global sensor_value_list
     for i in range(0,3):
-        sensor_value_list.append(float(temp_list[i]))
 
-    print(sensor_value_list)
+        sensor_value_list.append(float(temp_list[i]))
+    print(sensor_value_list)        
+
+#Master_subnode.connect(host,port,60)
 
 
 Master_subnode.on_connect = on_connect
 Master_subnode.on_message = on_message
 Master_subnode.connect(host,port,60)
 Master_subnode.loop_start()
-#Master_subnode.subscribe("Sensor_Values")
+#Master_subnode.subscribe("Sensor_Values")   
 
 while True:
     time.sleep(0.5)
     command = input("Enter Command :")
-
+   
 
     if (command == "1"):
         Master_subnode.publish("Sensor_Read_Request",command)
-        time.sleep(6)
+        print("Sd")
+        time.sleep(6) 
     if(command == "2"):
                                             #Decide which actuator to s$
-                                            #Write it in the form of a $
-        actuation_values = "1:On,2:Off,3:On,4:Off"
+        #global actuation_values                                    #Write it in the form of a $
+        actuation_values = "1111"
         Master_subnode.publish("Actuation",actuation_values)
+        time.sleep(5)  
+                                                    #Change actuation_values if needed
+
+
+#Master_subnode.loop_forever()
